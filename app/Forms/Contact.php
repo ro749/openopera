86e06967b5a8d3c8fe328bc9dca98a4f038d53e9
ro749 <45647699+ro749@users.noreply.github.com>
2025-10-17 12:09:37 -0600

    Update Contact.php

--- app/Forms/Contact.php
+++ app/Forms/Contact.php
@@ -2,18 +2,41 @@
 
 namespace App\Forms;
 
-use Ro749\OpenListingTemplate\Forms\Contact as ContactBase;
-use Ro749\SharedUtils\Forms\Selector;
-use App\Enums\UnitsStatus;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Ro749\SharedUtils\Forms\BaseForm;
+use Ro749\SharedUtils\Forms\FormField;
+use Ro749\SharedUtils\Forms\Selector;
+use Ro749\SharedUtils\Forms\InputType;
+use Ro749\SharedUtils\Forms\TextArea;
+use Ro749\OpenListingTemplate\Enums\UnitsStatus;
 use Ro749\OpenListingTemplate\Mail\ContactMail;
-class Contact extends ContactBase
+
+class Contact extends BaseForm
 {
     public function __construct()
     {
-        parent::__construct();
-        $this->fields['unit'] = Selector::fromDB(
+        parent::__construct(
+            submit_text: "Enviar",
+            uploading_message: "Enviando...",
+            success_msg: "¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.",
+            fields: [
+                'name' => new FormField(
+                    type: InputType::TEXT,
+                    label: 'Nombre',
+                    rules: ["required"]
+                ),
+                'email' => new FormField(
+                    type: InputType::EMAIL,
+                    label: 'Correo electrónico',
+                    rules: ["required"]
+                ),
+                'phone' => new FormField(
+                    type: InputType::PHONE,
+                    label: 'Teléfono',
+                    rules: ["required"]
+                ),
+                'unit' => Selector::fromDB(
                     id: 'unitselector',
                     table: "opera_depas",
                     label_column: "unidad",
@@ -23,7 +46,14 @@ class Contact extends ContactBase
                         $query->where('status', UnitsStatus::Disponible->value);
                     }
 
-                );
+                ),
+                'message' => new TextArea(
+                    label: 'Mensaje',
+                    rows: 3
+                ),
+
+            ],
+        );
     }
 
     public function prosses(Request $rawRequest): string
