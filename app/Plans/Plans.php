9aa35bdab192d548822b03576b6152d59fc89d19
ro749 <45647699+ro749@users.noreply.github.com>
2025-10-20 15:36:27 -0600

    Update Plans.php

--- app/Plans/Plans.php
+++ app/Plans/Plans.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 
 use Carbon\Carbon;
 use Ro749\ListingUtils\Plans\PlansBase;
-
+use Ro749\ListingUtils\Plans\Plan;
 class Plans extends PlansBase
 {   
     public function __construct()
@@ -15,7 +15,16 @@ class Plans extends PlansBase
             plans_table: 'opera_plans',
             lines_table: 'opera_planlines',
             title_column: 'nombre',
-            discount_column: 'descuento'
+            discount_column: 'descuento',
+            default_plan: new Plan(
+                id: 0,
+                title: 'Sin Plan',
+                discounts: 0,
+                lines: [],
+                price_tag: 'Precio',
+                total_tag: '',
+            ),
+
         );
     }
 }
